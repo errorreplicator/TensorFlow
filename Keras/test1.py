14d57b547db7a11cfd855eda9dddf7e0baaba63a
errorreplicator <error.replicator@gmail.com>
2018-11-05 15:28:46 +0100

    PC changes

--- Keras/test1.py
+++ Keras/test1.py
@@ -1,13 +1,13 @@
 import keras
-# import tensorflow as tf
+import tensorflow as tf
 import timer
 
 minst = keras.datasets.mnist
 
 (x_train,y_train), (x_test,y_test) = minst.load_data()
-print(len(x_train))
-print(len(x_train[0]))
-print(x_train[0])
+# print(len(x_train))
+# print(len(x_train[0]))
+# print(x_train[0])
 
 x_train = keras.utils.normalize(x_train,axis=1)
 x_test = keras.utils.normalize(x_test,axis=1)
@@ -32,3 +32,4 @@ model.fit(x_train,y_train,epochs=4)
 #step 2 -  784 neurons - dropauts 0.25: loss: 0.0637 - acc: 0.9798
 #step 3 -  128 neurons -
 #step 4 -  128 neurons - dropauts 0.25: Elapsed time: 0:00:49.117571
+#step 4a PC -  128 neurons - dropauts 0.25: Elapsed time: 0:00:22.729088
