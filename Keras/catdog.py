34b9fb56fbb229a49ea4deeb6e302d923535c065
errorreplicator <error.replicator@gmail.com>
2018-11-09 15:52:16 +0100

    with Conv layer

--- Keras/catdog.py
+++ Keras/catdog.py
@@ -2,15 +2,16 @@ import os
 import cv2
 import random
 import numpy as np
-# import keras
+import keras
 import matplotlib.pyplot as plt
+np.set_printoptions(linewidth=102)
 
 path = '../Data/'
-# catalogs = ['Dog','Cat']
-catalogs = ['test']
+catalogs = ['Dog','Cat']
+# catalogs = ['test']
 images = []
 input_size = 1000
-resolution = 100
+resolution = 50
 
 for x in catalogs:
     curr_catal = os.path.join(path,x)
@@ -18,85 +19,77 @@ for x in catalogs:
     class_num = catalogs.index(x)
     for file in os.listdir(curr_catal):
         index += 1
-        image = cv2.imread(os.path.join(curr_catal,file))
+        # print(os.path.join(curr_catal,file))
+
 
         try:
-            grey = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+            image = cv2.imread(os.path.join(curr_catal, file),cv2.IMREAD_GRAYSCALE)
+            image_resize = cv2.resize(image, (resolution, resolution))
+            images.append([image_resize, class_num])
+            # grey = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
         except Exception as e:
             pass
 
-        image_resize = cv2.resize(grey,(resolution,resolution))
-        images.append([image_resize,class_num])
+
         if index >= input_size: break
 
-# print(len(images))
+
+            # img = images[0][0][0]
+            # print(img)
+            # print(len(img))
+            # # print(len(images))
+random.seed(3)
 random.shuffle(images)
-images = np.asarray(images)
-# print(images[0])
-
-
+# print(len(images))
+print('shuffled table\n',images[0][0][0])
+# print(images[1][0][0])
+# print(images[2][0][0])
+            # images
+# = np.asarray(images)
+            # # print(images[0])
+            #
+            #
 X_train = []
 y_train = []
 
 for x,y in images:
     X_train.append(x)
     y_train.append(y)
-
-# print(y_train[0])
-# plt.imshow(images[0][0],cmap='gray' )
-# plt.show()
-#
-# print(y_train[1])
-# plt.imshow(images[1][0],cmap='gray' )
-# plt.show()
-
+print('after X created\n', X_train[0][0])
+            # print(y_train[0])
+            # plt.imshow(images[0][0],cmap='gray' )
+            # plt.show()
+            #
+            # print(y_train[1])
+            # plt.imshow(images[1][0],cmap='gray' )
+            # plt.show()
+# img = images[0][0] ###############################################
 X_train = np.array(X_train)
-X_train = X_train.reshape(input_size*2,resolution,resolution,1)
+X_train = X_train.reshape(len(images),resolution,resolution,1)
+print(X_train.shape)
+# print(y_train)
+print('reshape\n',X_train[0][0])
 
-    # model = keras.models.Sequential()
-    #
-    # model.add(keras.layers.Conv2D(128,(3,3),input_shape=(resolution,resolution,1)))
-    # model.add(keras.layers.Activation('relu'))
-    # model.add(keras.layers.MaxPool2D(pool_size=(2,2)))
-    #
-    # model.add(keras.layers.Conv2D(128,(3,3)))
-    # model.add(keras.layers.Activation('relu'))
-    # model.add(keras.layers.MaxPool2D(pool_size=(2,2)))
-    #
-    # model.add(keras.layers.Flatten())
-    # model.add(keras.layers.Dense(32))
-    # model.add(keras.layers.Activation('relu'))
-    #
-    # model.add(keras.layers.Dense(1))
-    # model.add(keras.layers.Activation('sigmoid'))
-    # #
-    # model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    # model.fit(X_train,y_train,epochs=5)
+X_train = X_train/255.0 #### WTF ?
+
+model = keras.models.Sequential()
+
+model.add(keras.layers.Conv2D(256,(3,3),input_shape=(resolution,resolution,1)))
+model.add(keras.layers.Activation('relu'))
+model.add(keras.layers.MaxPool2D(pool_size=(2,2)))
+
+model.add(keras.layers.Conv2D(256,(3,3)))
+model.add(keras.layers.Activation('relu'))
+model.add(keras.layers.MaxPool2D(pool_size=(2,2)))
+
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(64))
+model.add(keras.layers.Activation('relu'))
+
+model.add(keras.layers.Dense(1))
+model.add(keras.layers.Activation('sigmoid'))
+#
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(X_train,y_train,epochs=5)
 
 
-
-# from keras.models import Sequential
-# from keras.layers import Conv2D, Activation, MaxPooling2D, Flatten, Dense
-#
-# model = Sequential()
-#
-# model.add(Conv2D(128, (3, 3), input_shape=(resolution, resolution,1)))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-#
-# model.add(Conv2D(128, (3, 3)))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-#
-# model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-#
-# model.add(Dense(64))
-# model.add(Activation('relu'))
-# model.add(Dense(1))
-# model.add(Activation('sigmoid'))
-#
-# model.compile(loss='binary_crossentropy',
-#               optimizer='adam',
-#               metrics=['accuracy'])
-#
-# model.fit(X_train, y_train, batch_size=32, epochs=4, validation_split=0.3)
--- test.py
+++ test.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import os
 import cv2
 from tqdm import tqdm
+import pandas as pd
+import columnize
+
+np.set_printoptions(linewidth=102)
+
 
 DATADIR = "./Data/"
 
@@ -22,6 +27,7 @@ def create_training_data():
 
         for img in tqdm(os.listdir(path)):  # iterate over each image per dogs and cats
             index+=1
+            # print(os.path.join(path,img))
             try:
                 img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
                 new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
@@ -31,31 +37,39 @@ def create_training_data():
             if index>=input_size: break
 
 create_training_data()
-print(training_data)
-plt.imshow(training_data[0][0]) #Stage ONE - lets do it step by step
-plt.show()
+# print(training_data)
+# print(training_data[0][0][0])
+# print(type(training_data[0][0][0]))
+# print(columnize.columnize(training_data[0][0][0]),30)
+# plt.imshow(training_data[0][0]) #Stage ONE - lets do it step by step
+# plt.show()
 
 # print(len(training_data))
 
 
 
 import random
+random.seed(3)
 random.shuffle(training_data)
-
-# for sample in training_data[:10]:
-#     print(sample[1])
-
+print('shuffled table \n',training_data[0][0][0])
+# print(training_data[0][0][1])
+#
+# # for sample in training_data[:10]:
+# #     print(sample[1])
+#
 X = []
 y = []
-
+#
 for features,label in training_data:
     X.append(features)
     y.append(label)
-
-# print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE, 1))
-
+print('after X created \n',X[0][0])
+# # print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE, 1))
+#
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)# !!!!!!!!!!!!!!!!!!!!!################%CHECK this reshape with mine reshape !!!!!!!!!!!!@@@@@@@@@@@@%%%%%%%%%%%%%@@@@@@@@@@!!!!!!!
-
+print(X.shape)
+# print(y)
+print('reshape \n',X[0][0])
 
 
 
